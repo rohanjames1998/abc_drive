326c439d4661859552ae7a6d2b378976d43ab5d9
Rohan James <rohanjames9282@gmail.com>
2024-12-16 19:58:11 +0530

    Add logic to add name when creating new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :uploads
+
+  validates :email, presence:true, uniqueness: {case_sensitivity: false}, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i}
+  validates :name, presence: true
 end
