d223c59efd257ef580a39f1fca20514d3cd70ded
Rohan James <rohanjames9282@gmail.com>
2025-01-16 10:42:10 +0530

    Add upload limit

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -8,7 +8,7 @@ class Upload < ApplicationRecord
 
   validates :file_name, presence: true
   validates :file, presence: true
-
+  validate :validate_file_size
 
   def shortened_url
     super || create_default_shortened_url
@@ -16,7 +16,18 @@ class Upload < ApplicationRecord
 
   private
 
+
+  def validate_file_size
+    if file.attached?
+      if file.blob.byte_size > 100000
+        errors.add(:file, 'size is too big')
+      end
+    end
+  end
+
   def create_default_shortened_url
+    return unless file.attached?
+
     full_url = Rails.application.routes.url_helpers.url_for(file)
     unique_identifier = Digest::MD5.hexdigest(full_url)
     create_shortened_url(full_url: full_url, unique_identifier: unique_identifier)
